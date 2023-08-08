aef8bb48cd29107e8eebcb9b242765d78e414691
rajahwu <vmradford@gmail.com>
2023-08-08 14:55:06 -0400

    add types for likes table

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,32 @@ import { cookies } from "next/headers";
 import AuthButtonServer from "./auth-button-server";
 import { redirect } from "next/navigation";
 import NewTweet from "./new-tweet";
+import Likes from "./likes";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  const { 
-    data: { session }
+  const {
+    data: { session },
   } = await supabase.auth.getSession();
 
-
   if (!session) {
-    redirect('/login')
+    redirect("/login");
   }
 
-  const { data: tweets } = await supabase.from("tweets").select("*, profiles(*)");
+  const { data } = await supabase
+    .from("tweets")
+    .select("*, author: profiles(*), likes(user_id)");
+
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      author: Array.isArray(tweet.author) ? tweet.author[0] : tweet.author,
+      user_has_liked_tweet: !!tweet.likes.find(
+        (like) => like.user_id === session.user.id
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
 
   return (
     <div>
@@ -24,8 +36,11 @@ export default async function Home() {
       <NewTweet />
       {tweets?.map((tweet) => (
         <div key={tweet.id}>
-          <p>{tweet.profiles?.name} {tweet.profiles?.username}</p>
+          <p>
+            {tweet.author.name} {tweet.author.username}
+          </p>
           <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
         </div>
       ))}
     </div>
